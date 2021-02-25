74fc54610a8768d772d67d53c896ef5edb8ff0a5
allisson-faiad <allisson.faiad001@gmail.com>
2021-02-24 23:22:09 -0300

    logotipo finalizado e convertido para svg

--- Site/php/conecta_banco.php
+++ Site/php/conecta_banco.php
@@ -2,7 +2,7 @@
 define('servidor', 'localhost');
 define('user','root');
 define('senha', '');
-define('db', 'db_foreveryone2');
+define('db', 'db_foreveryone');
   
 $con = mysqli_connect(servidor,user,senha,db) or die('Não foi possivel conectar');
 date_default_timezone_set("America/Sao_Paulo");
